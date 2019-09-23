1a2fb756f2804561ecc4e633de8e37ffdc416b3e
Jack Makrides <jack.makrides@btinternet.com>
2019-09-23 17:51:11 +0100

    up to 1.9.6

--- 1.8/src/Circle.java
+++ 1.8/src/Circle.java
@@ -15,13 +15,21 @@ public class Circle {
         colour = "red";
     }
 
+    private Circle(double radius, String colour) {
+        this.radius = radius;
+        this.colour = colour;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a radius for the circle:");
         double userRadius = scanner.nextDouble();
-        Circle circle = new Circle(userRadius);
+        System.out.println("Please enter a number to define colour");
+        String userColour = setColour(scanner.nextDouble());
+        Circle circle = new Circle(userRadius, userColour);
         System.out.println(circle.getRadius());
         System.out.println(circle.getArea());
+        System.out.println(circle.getColour());
     }
 
     private double getRadius() {
@@ -32,15 +40,32 @@ public class Circle {
         return 3.14 * (radius * radius);
     }
 
-    private void setColour(double d) {
-        if (d < 10) {
+    private String getColour() {
+        return colour;
+    }
+
+    private static String setColour(double d) {
+        String colour;
+
+        if (d >= 0 && d <= 5) {
             colour = "red";
         }
-        else if (d >= 10 && d < 50) {
+        else if (d >= 6 && d <= 10) {
             colour = "amber";
         }
-        else {
+        else if (d >= 11 && d <= 15){
             colour = "green";
         }
+        else if (d >= 16 && d <= 20){
+            colour = "blue";
+        }
+        else if (d >= 21 && d <= 25){
+            colour = "yellow";
+        }
+        else {
+            colour = "purple";
+        }
+
+        return colour;
     }
 }
